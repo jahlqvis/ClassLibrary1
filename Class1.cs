ade3fa5699f8038652db5551a9ee9c01f4afcc15
Ahlqvist, Johan <johan.ahlqvist@wartsila.com>
2021-09-08 00:00:18 +0300

    Add project files.

---
+++ Class1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.InteropServices;
+
+namespace ClassLibrary1
+{
+    [Guid("77C507CE-E6D8-4780-B7E6-46526271CD05")]
+    [ComVisible(true)]
+    [ClassInterface(ClassInterfaceType.None)]
+    public class Class1 : IClass1
+    {
+        public double SumVector(double[] arr)
+        {
+            double sum = 0;
+            foreach(var d in arr)
+            {
+                sum += d;
+            }
+            return sum;
+        }
+    }
+}
---
+++ IClass1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.InteropServices;
+
+namespace ClassLibrary1
+{
+    [Guid("2E09C500-0C87-4EBF-A196-3D0E6770E55D")]
+    [ComVisible(true)]
+    [InterfaceType(ComInterfaceType.InterfaceIsDual)]
+    public interface IClass1
+    {
+        [DispId(1)]
+        double SumVector(double[] arr);
+    }
+}
